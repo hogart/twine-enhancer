23b7994752ee459a9486e9be361f95abe8488a9a
Konstantin Kitmanov <doctor.hogart@gmail.com>
2019-03-16 17:41:25 +0100

    integrate eslint into build system, update deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import html from 'rollup-plugin-html';
+import { eslint } from 'rollup-plugin-eslint';
 
 export default {
     input: 'content/index.js',
@@ -12,6 +13,10 @@ export default {
     plugins: [
         resolve(),
         commonjs(),
+        eslint({
+            exclude: ['**/*.html'],
+            formatter: 'unix',
+        }),
         html(),
     ],
 };
